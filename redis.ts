4b3b45580a32ed77cb0c0fe56b969d2a4a470320
Callum Alton <callumalton@PC11802.local>
2024-05-09 21:36:05 +0100

    fix env variables

--- redis.ts
+++ redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 import { env } from "process";
 
-const redis = new Redis(`${env.REDIS_URL}`);
+const redis = new Redis(`${env.NEXT_PUBLIC_REDIS_URL}`);
 
 export default redis;
