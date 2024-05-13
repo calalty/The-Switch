91f2550dcfad3874697448ac2f36bcc10f3ba74b
Callum Alton <callumalton@pc11802.home>
2024-05-13 21:56:45 +0100

    fix icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import { Session } from "next-auth";
 import Link from "next/link";
 import React from "react";
+import { UserIcon } from "@heroicons/react/24/solid";
 
 type HeaderProps = {
   session: Session | null;
@@ -18,15 +19,7 @@ export const Header = async ({ session }: HeaderProps) => {
       <div className="flex flex-row items-center gap-4">
         {session && (
           <div className="flex items-center gap-2">
-            <svg
-              viewBox="0 0 448 512"
-              fill="white"
-              height="1rem"
-              width="1rem"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path>
-            </svg>
+            <UserIcon className="h-6 w-6" />
             <p className="text-2xl font-black">{session.user?.name}</p>
           </div>
         )}
